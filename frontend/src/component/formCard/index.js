6b6f70d48b1255cc6ac66b3db715761fdf6f79cc
Fellipe A. de azevedo <fellipe.azevedo@kian.com.br>
2022-10-19 11:24:11 -0300

    limpeza do código gabarito, frontend

--- frontend/src/component/formCard/index.js
+++ frontend/src/component/formCard/index.js
@@ -1,32 +1,6 @@
 import React, { Component } from 'react'
-import ReactInputMask from 'react-input-mask'
 import './styles.css'
-import icon from './add.png'
 import axios from 'axios'
-
-
-const stGen = {
-    marginbottom: "-50px"
-}
-
-const stEnd1 = {
-    display: "flex",
-    justifyContent: "spaceBetween"
-}
-
-const stEnd2 = {
-    width: "60%",
-    paddingLeft: "0"
-}
-
-const stEnd3 = {
-    width: "40%",
-    paddingRight: "0"
-}
-
-const stEnd4 = {
-    left: "60%"
-}
 
 
 class Rca extends Component {
@@ -97,13 +71,6 @@ class Rca extends Component {
         this.setState({ rows })
     }
 
-    handleFile(e) {
-        let file = e.target.files[0]
-        this.setState({ file: file })
-
-
-    }
-
     onSubmit(e) {
         console.log(this.state, 'THE STATE --------- $$$$')
 
@@ -158,7 +125,8 @@ class Rca extends Component {
                             onChange={e => this.setState({ solicitante: e.target.value })} />
                         <label htmlFor="nome" className="labelInput">Nome do solicitante</label>
                     </div>
-                    <div className="inputBox" style={stGen}>
+                
+                    <div className="inputBox">
                         <div className="radio" value={this.state.sex}
                             onChange={e => this.setState({ sex: e.target.value })}>
                                 <label htmlFor="nome" className="labelInput">Você é:</label>
@@ -190,117 +158,6 @@ class Rca extends Component {
                         <label htmlFor="email" className="labelInput">E-mail</label>
                     </div>
 
-                    <div className="inputBox">
-                        <ReactInputMask mask="(99)99999-9999"
-                            type="text" name="tel"
-                            id="telefone"
-                            className="inputUser"
-                            autoComplete="off"
-                            value={this.state.tel}
-                            onChange={e => this.setState({ tel: e.target.value })} />
-                        <label htmlFor="telefone" className="labelInput">Telefone</label>
-                    </div>
-
-                    <div className="inputBox">
-                        <ReactInputMask mask="999.999.999-99"
-                            type="text" name="cpf" className="inputUser"
-                            id="input_cpf"
-                            autoComplete="off"
-                            value={this.state.cpf}
-                            onChange={e => this.setState({ cpf: e.target.value })} />
-                        <label htmlFor="cpf" className="labelInput">CPF</label>
-                    </div>
-
-                    <div className="inputBox">
-                        <ReactInputMask mask="99.999.999/9999-99" type="text" name="cnpj"
-                            id="cnpj" className="inputUser"
-                            value={this.state.cnpj}
-                            onChange={e => this.setState({ cnpj: e.target.value })} />
-                        <label htmlFor="cnpj" className="labelInput">CNPJ</label>
-                    </div>
-                    <div className="inputBox" style={stGen}>
-                        Sexo:
-                        <div className="radio" value={this.state.sex}
-                            onChange={e => this.setState({ sex: e.target.value })}>
-                            <div className="input_div">
-                                <label htmlFor="feminino">Feminino</label>
-                                <input type="radio" id="feminino" name="genero" value="feminino"
-                                />
-                            </div>
-
-                            <div className="input_div">
-                                <label htmlFor="masculino">Masculino</label>
-                                <input type="radio" id="masculino" name="genero" value="masculino" />
-                            </div>
-
-                            <div className="input_div">
-                                <label htmlFor="outros">Outro</label>
-                                <input type="radio" id="outros" name="genero" value="outros" />
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div id="nascimento" className="inputBox">
-                        <label htmlFor="datan_ascimento"><b>Data de Nascimento:</b></label>
-                        <input type="date"
-                            name="bDate"
-                            id="data_nascimento"
-                            value={this.state.bDate}
-                            onChange={e => this.setState({ bDate: e.target.value })} />
-                    </div>
-
-
-
-                    <div className="inputBox">
-                        <ReactInputMask mask="99999-999" type="text" name="cep"
-                            id="cep" className="inputUser"
-                            value={this.state.cep}
-                            onChange={e => this.setState({ cep: e.target.value })} />
-                        <label htmlFor="cep" className="labelInput">CEP</label>
-                    </div>
-
-                    <div className="inputBox" style={stEnd1}>
-                        <div style={stEnd2}>
-                            <input type="text" name="endereco" id="endereco" className="inputUser"
-                                autoComplete="off"
-                                value={this.state.endereco}
-                                onChange={e => this.setState({ endereco: e.target.value })} />
-                            <label htmlFor="endereco" className="labelInput">Endereço</label>
-                        </div>
-
-                        <div style={stEnd3}>
-                            <input type="number" name="num" id="numero" className="inputUser"
-                                autoComplete="off"
-                                value={this.state.num}
-                                onChange={e => this.setState({ num: e.target.value })} />
-                            <label htmlFor="numero" className="labelInput" style={stEnd4}>N°</label>
-                        </div>
-                    </div>
-
-                    <div className="inputBox">
-                        <input type="text" name="bairro" id="bairro" className="inputUser"
-                            autoComplete="off"
-                            value={this.state.bairro}
-                            onChange={e => this.setState({ bairro: e.target.value })} />
-                        <label htmlFor="bairro" className="labelInput">Bairro</label>
-                    </div>
-
-                    <div className="inputBox">
-                        <input type="text" name="cidade" id="cidade" className="inputUser"
-                            autoComplete="off"
-                            value={this.state.cidade}
-                            onChange={e => this.setState({ cidade: e.target.value })} />
-                        <label htmlFor="cidade" className="labelInput">Cidade</label>
-                    </div>
-
-                    <div className="inputBox">
-                        <input type="text" name="estado" id="estado" className="inputUser"
-                            autoComplete="off"
-                            value={this.state.estado}
-                            onChange={e => this.setState({ estado: e.target.value })} />
-                        <label htmlFor="estado" className="labelInput">Estado</label>
-                    </div>
 
                     <div className="row-container">
                         <h3>Produtos</h3>
@@ -370,24 +227,6 @@ class Rca extends Component {
                                         Deletar Último
                                     </button>
 
-
-
-                                    <div className="arquivos">
-                                        <div className="inputBox">
-                                            <span>Identidade (Anexo):*</span>
-                                            <div className="inputFile">
-
-                                                <label className="labelArquivos" htmlFor="identidade" id="identidade_L">
-                                                    <img className="imageInput" src={icon} alt="ident" />
-
-                                                </label>
-                                                <input type="file" id="identidade"
-
-                                                    onChange={(e) => this.handleFile(e)} />
-
-                                            </div>
-                                        </div>
-                                    </div>
                                 </div>
                             </div>
                         </div>
