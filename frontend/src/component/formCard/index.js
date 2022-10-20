4c449a9edd83c54371cc606b6f57e4451a33fe47
Fellipe A. de azevedo <fellipe.azevedo@kian.com.br>
2022-10-20 12:12:14 -0300

    Tabela de MPDV criada e implementada na função de selecção do 'rádio'.

--- frontend/src/component/formCard/index.js
+++ frontend/src/component/formCard/index.js
@@ -257,87 +257,168 @@ class Rca extends Component {
                     </div>
                 </div>
 
+
+                {/*//////////Aqui começa a TABELA de dados dos BRINDES///////////////////// */}
+
                 {solicitar === 'brinde' && (
                     <div>
-                        <div className="row-container">
-                            <h3>Produtos</h3>
-                            <div className="table-container">
-                                <div className="row clearfix">
-                                    <div >
-                                        <table
 
-                                            id="tab_logic"
-                                        >
-                                            <thead>
-                                                <tr>
+                        <h3>Informações do brinde</h3>
+                        <div className="tableContainer">
 
-                                                    <th className="text-center"> Item </th>
-                                                    <th className="text-center"> Quantidade </th>
+                            <table
 
+                                id="tab_logic"
+                            >
+                                <thead>
+                                    <tr>
+                                        <th className="text-center"> Item </th>
+                                        <th className="text-center"> Quantidade </th>
+                                    </tr>
+                                </thead>
 
-                                                </tr>
-                                            </thead>
+                                <tbody>
+                                    {this.state.itens.map((item, idx) => (
+                                        <tr id="addr0" key={idx}>
 
-                                            <tbody>
-                                                {this.state.itens.map((item, idx) => (
-                                                    <tr id="addr0" key={idx}>
+                                            <td>
+                                                <select
+                                                    type="text"
+                                                    name="nome"
+                                                    value={this.state.itens[idx].nome}
+                                                    onChange={this.handleChange(idx)}
+                                                    className="inputTable">
+                                                    <option></option>
+                                                    <option>Chaveiro Abridor | Rayco</option>
+                                                    <option>Chaveiro Lanterna | Rayco</option>
+                                                    <option>Ecobag Vermelha | Kian</option>
+                                                    <option>Sacola de Papel Rayco/kian</option>
+                                                    <option>Caixinha de Som</option>
+                                                    <option>Catálogo Rayco 2020</option>
+                                                    <option>Catálogo Kian 2021</option>
+                                                </select>
+                                            </td>
+                                            <td>
+                                                <input
+                                                    type="text"
+                                                    name="quantidade"
+                                                    value={this.state.itens[idx].quantidade}
+                                                    onChange={this.handleChangequantidade(idx)}
+                                                    className="inputTable2" />
+                                            </td>
 
-                                                        <td>
-                                                            <select
-                                                                type="text"
-                                                                name="nome"
-                                                                value={this.state.itens[idx].nome}
-                                                                onChange={this.handleChange(idx)}
-                                                                className="inputTable">
-                                                                <option>Chaveiro Abridor | Rayco</option>
-                                                                <option>Chaveiro Lanterna | Rayco</option>
-                                                                <option>Ecobag Vermelha | Kian</option>
-                                                                <option>Sacola de Papel Rayco/kian</option>
-                                                                <option>Caixinha de Som</option>
-                                                                <option>Catálogo Rayco 2020</option>
-                                                                <option>Catálogo Kian 2021</option>
-                                                                </select>
-                                                        </td>
-                                                        <td>
-                                                            <input
-                                                                type="text"
-                                                                name="quantidade"
-                                                                value={this.state.itens[idx].quantidade}
-                                                                onChange={this.handleChangequantidade(idx)}
-                                                                className="inputTable2"
-                                                            />
-                                                        </td>
+                                            <td>
+                                                <button
+                                                    className="btn btn-outline-danger btn-sm"
+                                                    onClick={this.handleRemoveRow}>
+                                                    Remove
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
 
-                                                        <td>
-                                                            <button
-                                                                className="btn btn-outline-danger btn-sm"
-                                                                onClick={this.handleRemoveRow}
-                                                            >
-                                                                Remove
-                                                            </button>
-                                                        </td>
-                                                    </tr>
-                                                ))}
-                                            </tbody>
-                                        </table>
-
-                                        <button onClick={this.handleAddRow} className="btn btn-primary">
-                                            Adicionar
-                                        </button>
-                                        <button
-                                            onClick={this.handleRemoveRow}
-                                            className="btn btn-danger float-right"
-                                        >
-                                            Deletar Último
-                                        </button>
-
-                                    </div>
-                                </div>
-                            </div>
+                            <button onClick={this.handleAddRow} className="btnAdicionar">
+                                Adicionar
+                            </button>
+                            <button
+                                onClick={this.handleRemoveRow}
+                                className="btnDeletarUltimo">
+                                Deletar Último
+                            </button>
                         </div>
                     </div>
                 )}
 
+                {/*//////////Aqui termina a TABELA de dados dos BRINDES///////////////////// */}
+
+                {/*//////////Aqui começa a TABELA de dados dos MPDV///////////////////// */}
+
+                {solicitar === 'mpdv' && (
+                    <div>
+
+                        <h3>MPDV</h3>
+                        <div className="tableContainer">
+
+                            <table
+                                id="tab_logic">
+                                <thead>
+                                    <tr>
+                                        <th className="text-center"> MPDV solicitado </th>
+                                        <th className="text-center"> Quantidade </th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    {this.state.itens.map((item, idx) => (
+                                        <tr id="addr0" key={idx}>
+
+                                            <td>
+                                                <select
+                                                    type="text"
+                                                    name="nome"
+                                                    value={this.state.itens[idx].nome}
+                                                    onChange={this.handleChange(idx)}
+                                                    className="inputTable">
+                                                    <option></option>
+                                                    <option>8904 – EXPOSITOR MANGUEIRA LUM C/6 SUP KIAN</option>
+                                                    <option>8905 – DISPLAY BALCAO SIMPL 6 GANCHOS</option>
+                                                    <option>9053 - CESTO EM PLASTIONDAS 0,74X1,0 C/DIVISAO</option>
+                                                    <option>10157 - EXPOSITOR CHAO SIMPL C/20G C/ TEST KIAN</option>
+                                                    <option>10053 - TESTADOR PEQUENO P/ LAMP. E-27 KIAN</option>
+                                                    <option>10517 - PLASTICO FORRACAO 40CMX25M KIAN</option>
+                                                    <option>13196 - EXPOSITOR ARAMADO 25G  C/CESTO RAYCO</option>
+                                                    <option>13197 - EXPOSITOR SLAT WALL  MDF  25G RAYCO</option>
+                                                    <option>13198 - EXPOSITOR DE BROCA RAYCO</option>
+                                                    <option>14254 - TESTADOR PEQUENO P/ LAMP C/ 5 BASES</option>
+                                                    <option>14255 - PLASTICO FORRACAO 40CMX50M KIAN</option>
+                                                    <option>9056 - CLIP STRIP 1M EM PVC FLEXIVEL</option>
+                                                    <option>9054 - STOPER 0,15 X 0 30 CM</option>
+                                                    <option>9059 - WOBBLER 10 CM FLEXÍVEL</option>
+                                                    <option>10518 - ADESIVO DE CHÃO </option>
+                                                    <option>15305 - EXPOSITOR DE CHÃO PILHAS</option>
+                                                    <option>15304 - EXPOSITOR BACÃO PILHAS</option>
+                                                    <option>15303 - CLIP STRIP PILHA</option>
+                                                </select>
+                                            </td>
+                                            <td>
+                                                <input
+                                                    type="text"
+                                                    name="quantidade"
+                                                    value={this.state.itens[idx].quantidade}
+                                                    onChange={this.handleChangequantidade(idx)}
+                                                    className="inputTable2"
+                                                />
+                                            </td>
+
+                                            <td>
+                                                <button
+                                                    className="btn btn-outline-danger btn-sm"
+                                                    onClick={this.handleRemoveRow}
+                                                >
+                                                    Remove
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+
+                            <button onClick={this.handleAddRow} className="btnAdicionar">
+                                Adicionar
+                            </button>
+                            <button
+                                onClick={this.handleRemoveRow}
+                                className="btnDeletarUltimo"
+                            >
+                                Deletar Último
+                            </button>
+
+                        </div>
+
+                    </div>
+                )}
 
                 <input type="submit" onClick={(e) => this.onSubmit(e)} />
 
