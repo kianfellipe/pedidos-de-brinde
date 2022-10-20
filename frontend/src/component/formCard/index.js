7ce48cbcebf8e2cb80bf0db85435ffa4b2beedbf
Fellipe A. de azevedo <fellipe.azevedo@kian.com.br>
2022-10-20 09:59:21 -0300

    Função de tabela criado, tabela muda de acordo com o tipo de solicitação, também foram feitos ajustes gerais no frontend

--- frontend/src/component/formCard/index.js
+++ frontend/src/component/formCard/index.js
@@ -82,8 +82,10 @@ class Rca extends Component {
             codigo: this.state.codigo,
             cargo: this.state.cargo,
             clienteNovo: this.state.clienteNovo,
+            clientCodigo: this.state.clientCodigo,
             razaoSocial: this.state.razaoSocial,
-            solicitar: this.state.solicitar
+            solicitar: this.state.solicitar,
+            itens: this.state.itens
 
         })], {
             type: 'application/json'
@@ -104,6 +106,7 @@ class Rca extends Component {
 
     render() {
         const { cargo } = this.state;
+        const { solicitar } = this.state;
         return (
             <div className="box">
                 <legend className="title">Solicitação de Brinde e MPDV</legend>
@@ -117,19 +120,19 @@ class Rca extends Component {
                 </div>
 
                 <div className="inputBox">
-                    <label htmlFor="nomeSolicitante" className="labelInput">Nome do solicitante</label>
                     <input type="text"
                         name="nomeSolicitante"
                         className="inputUser"
                         autoComplete="off"
                         value={this.state.nomeSolicitante}
                         onChange={e => this.setState({ nomeSolicitante: e.target.value })} />
+                    <label htmlFor="nomeSolicitante" className="labelInput">*Nome do solicitante</label>
                 </div>
 
                 <div className="inputRadioBox">
                     <div className="radio" value={this.state.cargo}
                         onChange={e => this.setState({ cargo: e.target.value })}>
-                        <label htmlFor="nome" className="labelInput">Você é:</label>
+                        <label>*Você é:</label>
                         <div className="input_div">
                             <label htmlFor="gerente">Gerente</label>
                             <input type="radio" id="gerente" name="area" value="gerente" onChange={this.onchange}
@@ -148,7 +151,6 @@ class Rca extends Component {
                     </div>
                 </div>
 
-
                 {cargo === 'representante' && (
                     <div className="inputBox">
                         <input type="text"
@@ -157,7 +159,7 @@ class Rca extends Component {
                             autoComplete="off"
                             value={this.state.codigo}
                             onChange={e => this.setState({ codigo: e.target.value })} />
-                        <label htmlFor="codigo" className="labelInput">Código</label>
+                        <label htmlFor="codigo" className="labelInput">*Código</label>
                     </div>
                 )}
 
@@ -169,7 +171,7 @@ class Rca extends Component {
                             autoComplete="off"
                             value={this.state.codigo}
                             onChange={e => this.setState({ codigo: e.target.value })} />
-                        <label htmlFor="codigo" className="labelInput">Código</label>
+                        <label htmlFor="codigo" className="labelInput">*Código</label>
                     </div>
                 )}
 
@@ -180,7 +182,7 @@ class Rca extends Component {
                         autoComplete="off"
                         value={this.state.email}
                         onChange={e => this.setState({ email: e.target.value })} />
-                    <label htmlFor="email" className="labelInput">E-mail</label>
+                    <label htmlFor="email" className="labelInput">*E-mail</label>
                 </div>
 
 
@@ -188,111 +190,155 @@ class Rca extends Component {
 
 
                 {/*//////////Aqui começa a solicitação de dados do cliente //////////////////////////*/}
-                <div className='inputDados'>
+                <div className='inputDados2'>
                     <b>Dados do cliente</b>
                     <br></br>
                     <p>Aqui você deve informar os dados do cliente para que vai receber os itens solicitados.</p>
                 </div>
 
-                <div className="inputBox">
-                    <div className="radio" value={this.state.cargo}
-                        onChange={e => this.setState({ cargo: e.target.value })}>
-                        <label htmlFor="nome" className="labelInpu">Cliente novo?</label>
+                <div className="inputRadioBox2">
+                    <div className="radio" value={this.state.clienteNovo}
+                        onChange={e => this.setState({ clienteNovo: e.target.value })}>
+                        <label htmlFor="nome" className="labelInpu">*Cliente novo?</label>
                         <div className="input_div">
                             <label htmlFor="gerente">Sim</label>
                             <input type="radio" id="gerente" name="clienteNovo" value="Sim" />
                         </div>
-
                         <div className="input_div">
                             <label htmlFor="Representante">Não</label>
                             <input type="radio" id="Representante" name="clienteNovo" value="Não" />
                         </div>
-
-
                     </div>
                 </div>
 
                 <div className="inputBox">
-                    <label htmlFor="nomeSolicitante" className="labelInput">Nome do solicitante</label>
+                    <input type="text"
+                        name="nomeSolicitante"
+                        className="inputUser2"
+                        autoComplete="off"
+                        value={this.state.clientCodigo}
+                        onChange={e => this.setState({ clientCodigo: e.target.value })} />
+                    <label htmlFor="nomeSolicitante" className="labelInput">*Código do Cliente</label>
+                </div>
+
+                <div className="inputBox">
                     <input type="text"
                         name="nomeSolicitante"
                         className="inputUser"
                         autoComplete="off"
-                        value={this.state.nomeSolicitante}
-                        onChange={e => this.setState({ nomeSolicitante: e.target.value })} />
+                        value={this.state.razaoSocial}
+                        onChange={e => this.setState({ razaoSocial: e.target.value })} />
+                    <label htmlFor="nomeSolicitante" className="labelInput">*Razão Social</label>
                 </div>
 
+                {/*//////////Aqui termina a solicitação de dados do cliente///////////////////// */}
 
+                {/*//////////Aqui começa a solicitação de dados dos produtos///////////////////// */}
 
-                <div className="row-container">
-                    <h3>Produtos</h3>
-                    <div className="table-container">
-                        <div className="row clearfix">
-                            <div className="col-md-12 column">
-                                <table
-                                    className="table table-bordered table-hover"
-                                    id="tab_logic"
-                                >
-                                    <thead>
-                                        <tr>
+                <div className="inputRadioBox">
+                    <div className="radio" value={this.state.solicitar}
+                        onChange={e => this.setState({ solicitar: e.target.value })}>
+                        <label>*Solicitar:</label>
+                        <div className="input_div">
+                            <label htmlFor="brinde">Brinde</label>
+                            <input type="radio" id="brinde" name="area" value="brinde" onChange={this.onchange}
+                            />
+                        </div>
 
-                                            <th className="text-center"> Item </th>
-                                            <th className="text-center"> Quantidade </th>
+                        <div className="input_div">
+                            <label htmlFor="mpdv">MPDV</label>
+                            <input type="radio" id="mpdv" name="area" value="mpdv" onChange={this.onchange} />
+                        </div>
 
-
-                                        </tr>
-                                    </thead>
-
-                                    <tbody>
-                                        {this.state.itens.map((item, idx) => (
-                                            <tr id="addr0" key={idx}>
-
-                                                <td className="col-3">
-                                                    <input
-                                                        type="text"
-                                                        name="nome"
-                                                        value={this.state.itens[idx].nome}
-                                                        onChange={this.handleChange(idx)}
-                                                        className="form-control"
-                                                    />
-                                                </td>
-                                                <td className="col-2">
-                                                    <input
-                                                        type="text"
-                                                        name="quantidade"
-                                                        value={this.state.itens[idx].quantidade}
-                                                        onChange={this.handleChangequantidade(idx)}
-                                                        className="form-control"
-                                                    />
-                                                </td>
-
-                                                <td>
-                                                    <button
-                                                        className="btn btn-outline-danger btn-sm"
-                                                        onClick={this.handleRemoveRow}
-                                                    >
-                                                        Remove
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-
-                                <button onClick={this.handleAddRow} className="btn btn-primary">
-                                    Adicionar
-                                </button>
-                                <button
-                                    onClick={this.handleRemoveRow}
-                                    className="btn btn-danger float-right"
-                                >
-                                    Deletar Último
-                                </button>
-
-                            </div>
+                        <div className="input_div">
+                            <label htmlFor="personalizado">Personalizados</label>
+                            <input type="radio" id="personalizado" name="area" value="personalizado" onChange={this.onchange} />
                         </div>
                     </div>
                 </div>
+
+                {solicitar === 'brinde' && (
+                    <div>
+                        <div className="row-container">
+                            <h3>Produtos</h3>
+                            <div className="table-container">
+                                <div className="row clearfix">
+                                    <div >
+                                        <table
+
+                                            id="tab_logic"
+                                        >
+                                            <thead>
+                                                <tr>
+
+                                                    <th className="text-center"> Item </th>
+                                                    <th className="text-center"> Quantidade </th>
+
+
+                                                </tr>
+                                            </thead>
+
+                                            <tbody>
+                                                {this.state.itens.map((item, idx) => (
+                                                    <tr id="addr0" key={idx}>
+
+                                                        <td>
+                                                            <select
+                                                                type="text"
+                                                                name="nome"
+                                                                value={this.state.itens[idx].nome}
+                                                                onChange={this.handleChange(idx)}
+                                                                className="inputTable">
+                                                                <option>Chaveiro Abridor | Rayco</option>
+                                                                <option>Chaveiro Lanterna | Rayco</option>
+                                                                <option>Ecobag Vermelha | Kian</option>
+                                                                <option>Sacola de Papel Rayco/kian</option>
+                                                                <option>Caixinha de Som</option>
+                                                                <option>Catálogo Rayco 2020</option>
+                                                                <option>Catálogo Kian 2021</option>
+                                                                </select>
+                                                        </td>
+                                                        <td>
+                                                            <input
+                                                                type="text"
+                                                                name="quantidade"
+                                                                value={this.state.itens[idx].quantidade}
+                                                                onChange={this.handleChangequantidade(idx)}
+                                                                className="inputTable2"
+                                                            />
+                                                        </td>
+
+                                                        <td>
+                                                            <button
+                                                                className="btn btn-outline-danger btn-sm"
+                                                                onClick={this.handleRemoveRow}
+                                                            >
+                                                                Remove
+                                                            </button>
+                                                        </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+
+                                        <button onClick={this.handleAddRow} className="btn btn-primary">
+                                            Adicionar
+                                        </button>
+                                        <button
+                                            onClick={this.handleRemoveRow}
+                                            className="btn btn-danger float-right"
+                                        >
+                                            Deletar Último
+                                        </button>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
+
+
                 <input type="submit" onClick={(e) => this.onSubmit(e)} />
 
 
